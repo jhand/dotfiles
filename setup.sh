45d7b1c83cbd0258a7321cfea8779e43a2c6a1f1
jhand <manu.zeru@gmail.com>
2012-06-15 01:12:09 -0700

    Rearrange directory

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir ~/.xmonad
+ln -s ~/.dotfiles/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s ~/.dotfiles/xmonad/.xmonad.start.Xubuntu ~/.xmonad/xmonad.start
+ln -s ~/.dotfiles/xmonad/.xmobarrc ~/.xmonad/.xmobarrc
+
+ln -s ~/.dotfiles/.Xdefaults ~/.Xdefaults
+
+ln -s ~/.dotfiles/awesome/rc.lua ~/.config/awesome/rc.lua
+
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,61 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig
+
+import qualified Data.Map as M  
+import XMonad.Actions.CycleWS
+import XMonad.Layout.IndependentScreens
+ 
+keysToAdd x = [((mod4Mask, xK_F4), kill)
+     ,((mod4Mask, xK_Left), prevWS )
+     ,((mod4Mask, xK_Right), nextWS )
+     ,((mod4Mask, xK_d), spawn "firefox" ) 
+     ,((mod4Mask, xK_f), spawn "thunar" ) 
+     ,((mod4Mask .|. shiftMask, xK_f), spawn "pcmanfm" ) 
+     , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+     , ((0, xK_Print), spawn "scrot")
+     ,((mod4Mask, xK_s), spawn "medit" ) ]
+   
+newKeys x = M.union (keys defaultConfig x) (M.fromList (keysToAdd x)) -- to include new keys to existing keys  
+   
+-- Mouse bindings
+--
+-- Focus rules
+-- True if your focus should follow your mouse cursor.
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+myTerminal = "urxvt"
+myWorkspaces    = ["1:www","2:term","3:dev","4:IM","5","6","7","8","9"]
+
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+      , resource =? "Dialog" --> doFloat
+    ]
+ 
+main = do
+    nScreens <-  countScreens
+    spawn "sh ~/.xmonad/xmonad.start"
+    xmproc <- spawnPipe "xmobar ~/.xmonad/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+                        <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+			, ppHiddenNoWindows = xmobarColor "grey" ""
+			, ppHidden = xmobarColor "red" ""
+			, ppVisible =  xmobarColor "orange" "" . wrap "(" ")"
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+	, terminal = myTerminal
+	, workspaces = myWorkspaces
+        , focusFollowsMouse = myFocusFollowsMouse
+	, keys = newKeys
+	}
