78e4544952cb46a71fc2becef43e0baabcb15abe
jhand <manu.zeru@gmail.com>
2012-06-22 21:29:21 -0700

    fixed path for awesome autostart link

--- setup.Arch.sh
+++ setup.Arch.sh
@@ -2,13 +2,13 @@
 
 mkdir ~/.xmonad
 ln -s ~/.dotfiles/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-ln -s ~/.dotfiles/autostart.Arch ~/.xmonad/xmonad.start
+ln -s ~/.dotfiles/autostart.Arch ~/.xmonad/autostart.sh
 ln -s ~/.dotfiles/xmonad/.xmobarrc ~/.xmonad/.xmobarrc
 
 ln -s ~/.dotfiles/.Xdefaults ~/.Xdefaults
 
 mkdir ~/.config/awesome/
 ln -s ~/.dotfiles/awesome/rc.lua ~/.config/awesome/rc.lua
-ln -s ~/.dotfiles/autostart.Arch ~/.xmonad/autostart.sh
+ln -s ~/.dotfiles/autostart.Arch ~/.config/awesome/autostart.sh
 
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
