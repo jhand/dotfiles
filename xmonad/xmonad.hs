298e7c0b900b728789813c804bcc43074fe6ef9b
jhand <manu.zeru@gmail.com>
2012-06-29 02:39:50 -0700

    added proper dmenu command for keyboard

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -65,7 +65,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
  
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+    , ((modm,               xK_p     ), spawn "dmenu_run")
  
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
